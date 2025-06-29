99047770e047b77031b8eaf9ba504daaf976064e
Ankit Baghel <144383065+AnkitBaghel93@users.noreply.github.com>
2025-06-29 18:50:35 +0530

    Update Signup.jsx

--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
    e.preventDefault();
    try {
-     const res = await fetch('http://localhost:5000/api/signup', {
+     const res = await fetch('https://taskdistributor-backend-ukj6.onrender.com/api/signup', {
        method: 'POST',
        headers: { 'Content-Type': 'application/json' },
        body: JSON.stringify(formData),
